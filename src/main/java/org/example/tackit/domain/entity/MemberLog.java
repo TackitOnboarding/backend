be781279a40a08ecbe4b82d668e5f6d752694ba2
yeongsinkeem <dudtls0522@sookmyung.ac.kr>
2026-01-24 10:42:38 +0900

    refactor: db내 user 키워드 -> member로 모두 통일

--- src/main/java/org/example/tackit/domain/entity/MemberLog.java
+++ src/main/java/org/example/tackit/domain/entity/MemberLog.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class UserLog {
+public class MemberLog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
