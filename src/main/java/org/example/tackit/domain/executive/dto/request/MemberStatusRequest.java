da067907561b8f80aa07e15de2c42f24dfcfd7de
yeongsinkeem <dudtls0522@sookmyung.ac.kr>
2026-02-18 17:38:21 +0900

    refactor: #189-미사용 메서드 정리

--- src/main/java/org/example/tackit/domain/executive/dto/request/MemberStatusRequest.java
+++
@@ -1,8 +0,0 @@
-package org.example.tackit.domain.executive.dto.request;
-
-import lombok.Getter;
-
-@Getter
-public class MemberStatusRequest {
-    private String status;
-}
--- src/main/java/org/example/tackit/domain/executive/dto/response/MemberDetailResponse.java
+++
@@ -1,4 +0,0 @@
-package org.example.tackit.domain.executive.dto.response;
-
-public class MemberDetailResponse {
-}
