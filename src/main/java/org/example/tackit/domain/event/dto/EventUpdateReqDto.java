fb01a136a017a8e2df2f5a0f0c334298818ef809
tishakong <leehn45@naver.com>
2026-02-26 22:09:02 +0900

    refactor: ColorChip Enum 적용
    - 프론트엔드와 협의 후 기존 헥사 코드로 주고 받도록 구현되어있던 colorChip을 enum으로 리팩토링했습니다.
    - @JsonCreator와 @JsonValue를 활용해 프론트엔드에서는 소문자로, 백엔드에서는 대문자로 관리해도 자동 변환되도록 했습니다.

--- src/main/java/org/example/tackit/domain/event/dto/EventUpdateReqDto.java
+++ src/main/java/org/example/tackit/domain/event/dto/EventUpdateReqDto.java
@@ -1,21 +1,26 @@
 package org.example.tackit.domain.event.dto;
 
-import lombok.*;
-import org.example.tackit.domain.entity.EventScope;
-
 import java.time.LocalDateTime;
 import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.tackit.domain.entity.ColorChip;
+import org.example.tackit.domain.entity.EventScope;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
 public class EventUpdateReqDto {
-    private String title;
-    private LocalDateTime startsAt;
-    private LocalDateTime endsAt;
-    private String description;
-    private EventScope eventScope;
-    private List<Long> participants;
-    private String colorChip;
+
+  private String title;
+  private LocalDateTime startsAt;
+  private LocalDateTime endsAt;
+  private String description;
+  private EventScope eventScope;
+  private List<Long> participants;
+  private ColorChip colorChip;
 }
