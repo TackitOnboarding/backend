f4273f15c5ad80ca982d4e2dee2755e4a19bdd85
tishakong <leehn45@naver.com>
2026-03-03 00:43:27 +0900

    feat: 게시글 및 댓글 API 관련 DTO 구현

---
+++ src/main/java/org/example/tackit/domain/post/dto/PostDetailResDto.java
@@ -0,0 +1,37 @@
+package org.example.tackit.domain.post.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.tackit.domain.memberOrg.dto.SimpleMemberProfileDto;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostDetailResDto {
+
+  private PostInfo post;
+  private int totalCommentCount;
+  private List<CommentResDto> comments;
+
+  @Getter
+  @NoArgsConstructor
+  @AllArgsConstructor
+  @Builder
+  public static class PostInfo {
+
+    private Long id;
+    private String title;
+    private SimpleMemberProfileDto writer;
+    private String content;
+    private boolean isAnonymous;
+    private boolean isMine;
+    private boolean isScrap;
+    private int viewCount;
+    private LocalDateTime createdAt;
+  }
+}
---
+++ src/main/java/org/example/tackit/domain/post/dto/PostSummaryResDto.java
@@ -0,0 +1,26 @@
+package org.example.tackit.domain.post.dto;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.tackit.domain.entity.post.PostCategory;
+import org.example.tackit.domain.memberOrg.dto.SimpleMemberProfileDto;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostSummaryResDto {
+
+  private Long id;
+  private String title;
+  private String contentSummary;
+  private PostCategory postCategory;
+  private String thumbnail;
+  private SimpleMemberProfileDto writer;
+  private LocalDateTime createdAt;
+  private boolean isAnonymous;
+  private int viewCount;
+}
