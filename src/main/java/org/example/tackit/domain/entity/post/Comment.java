69da42478c4b1d76bf8040d8553d206f22b3fbbf
tishakong <leehn45@naver.com>
2026-03-02 22:14:48 +0900

    refactor: 대댓글 기능 반영할 수 있도록 댓글 엔티티 수정

--- src/main/java/org/example/tackit/domain/entity/post/Comment.java
+++ src/main/java/org/example/tackit/domain/entity/post/Comment.java
@@ -3,17 +3,27 @@ package org.example.tackit.domain.entity.post;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.tackit.domain.entity.ActiveStatus;
 import org.example.tackit.domain.entity.org.MemberOrg;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
@@ -31,10 +41,49 @@ public class Comment {
   @JoinColumn(name = "post_id", nullable = false)
   private Post post;
 
+  @Lob
   @Column(nullable = false, columnDefinition = "TEXT")
   private String content;
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "member_org_id")
-  private MemberOrg member;
+  private MemberOrg writer;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "parent_id")
+  private Comment parent;
+
+  @OneToMany(mappedBy = "parent", orphanRemoval = true)
+  private final List<Comment> children = new ArrayList<>();
+
+  @Enumerated(EnumType.STRING)
+  private ActiveStatus activeStatus = ActiveStatus.ACTIVE;
+
+  @CreatedDate
+  private LocalDateTime createdAt;
+
+  @Builder
+  public Comment(String content, Post post, MemberOrg writer, Comment parent) {
+    this.content = content;
+    this.post = post;
+    this.writer = writer;
+    this.parent = parent;
+  }
+
+  public void addReply(Comment reply) {
+    this.children.add(reply);
+    reply.setParent(this);
+  }
+
+  private void setParent(Comment parent) {
+    this.parent = parent;
+  }
+
+  public void update(String content) {
+    this.content = content;
+  }
+
+  public void delete() {
+    this.activeStatus = ActiveStatus.DELETED;
+  }
 }
