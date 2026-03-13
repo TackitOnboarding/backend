eb7f7118fff111c0f12ab909dcb1ed3f5de3c535
tishakong <leehn45@naver.com>
2026-03-13 20:48:02 +0900

    feat: 게시글 신고, 댓글 신고 기능 구현

--- src/main/java/org/example/tackit/domain/post/repository/CommentRepository.java
+++ src/main/java/org/example/tackit/domain/post/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package org.example.tackit.domain.post.repository;
 
 import java.util.List;
+import java.util.Optional;
 import org.example.tackit.domain.entity.post.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,4 +11,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
   @Query("SELECT c FROM Comment c JOIN FETCH c.writer WHERE c.post.id = :postId ORDER BY c.createdAt ASC")
   List<Comment> findAllByPostId(@Param("postId") Long postId);
+
+  @Query("SELECT c FROM Comment c JOIN FETCH c.post WHERE c.id = :commentId")
+  Optional<Comment> findByIdWithPost(@Param("commentId") Long commentId);
 }
