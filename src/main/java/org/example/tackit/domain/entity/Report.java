4d3b136a7a7102a3810f1fbe891c399b3550c079
tishakong <leehn45@naver.com>
2026-03-13 20:22:53 +0900

    refacor: Report 객체 내 from 함수 삭제 및 builder 구현

--- src/main/java/org/example/tackit/domain/entity/Report.java
+++ src/main/java/org/example/tackit/domain/entity/Report.java
@@ -14,21 +14,17 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.tackit.domain.entity.org.MemberOrg;
 import org.example.tackit.domain.entity.post.PostType;
-import org.example.tackit.domain.report.dto.ReportRequestDto;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
-@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 @Table(name = "report")
 public class Report {
@@ -86,17 +82,21 @@ public class Report {
   @Enumerated(EnumType.STRING)
   private ActiveStatus activeStatus;
 
-  //TODO Entity 안에 DTO를 참조하는 메소드가 있는 것이 이상함. 분리해야할듯
-  public static Report from(ReportRequestDto dto, MemberOrg reporter, MemberOrg targetMember) {
-    return Report.builder()
-        .reporter(reporter)
-        .targetMember(targetMember)
-        .targetId(dto.getTargetId())
-        .targetType(dto.getTargetType())
-        .reportReason(dto.getReason())
-        .activeStatus(ActiveStatus.ACTIVE) // 초기값 설정
-        .build();
+  @Builder
+  public Report(MemberOrg reporter, MemberOrg targetMember, Long targetId, TargetType targetType,
+      Long postId, PostType postType, String reportedPostTitle, String reportedContent,
+      ReportReason reportReason, String detailReason, ActiveStatus activeStatus) {
+    this.reporter = reporter;
+    this.targetMember = targetMember;
+    this.targetId = targetId;
+    this.targetType = targetType;
+    this.postId = postId;
+    this.postType = postType;
+    this.reportedPostTitle = reportedPostTitle;
+    this.reportedContent = reportedContent;
+    this.reportReason = reportReason;
+    this.detailReason = detailReason;
+    this.activeStatus = (activeStatus != null) ? activeStatus : ActiveStatus.ACTIVE;
   }
-
 }
 
