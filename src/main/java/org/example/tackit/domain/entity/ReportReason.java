0c3f965771088ee6678e95096f4b4107c3264a56
tishakong <leehn45@naver.com>
2026-03-13 20:53:42 +0900

    refactor: 기획과 동일하게 ReportReason 설명 수정

--- src/main/java/org/example/tackit/domain/entity/ReportReason.java
+++ src/main/java/org/example/tackit/domain/entity/ReportReason.java
@@ -1,20 +1,20 @@
 package org.example.tackit.domain.entity;
 
 public enum ReportReason {
-    ADVERTISEMENT("광고 및 홍보성 게시물"),
-    DUPLICATE("중복 또는 도배성 게시물"),
-    FALSE_INFO("허위 정보 또는 사실 왜곡"),
-    IRRELEVANT("게시판 주제와 관련 없는 내용"),
-    ETC("기타");
+  ADVERTISEMENT("광고 및 홍보성 내용"),
+  DUPLICATE("중복 또는 도배성 내용"),
+  FALSE_INFO("허위 정보 또는 사실 왜곡"),
+  IRRELEVANT("게시판 주제와 관련 없는 내용"),
+  ETC("기타");
 
-    private final String description;
+  private final String description;
 
-    ReportReason(String description) {
-        this.description = description;
-    }
+  ReportReason(String description) {
+    this.description = description;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public String getDescription() {
+    return description;
+  }
 }
 
