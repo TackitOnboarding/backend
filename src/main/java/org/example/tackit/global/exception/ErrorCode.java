06c2c64cca5c661731f0208f418f1a3e97770824
yeongsinkeem <dudtls0522@sookmyung.ac.kr>
2026-04-03 11:54:00 +0900

    신고 api 관련 에러코드 추가

--- src/main/java/org/example/tackit/global/exception/ErrorCode.java
+++ src/main/java/org/example/tackit/global/exception/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
     UNIVERSITY_NOT_FOUND(HttpStatus.NOT_FOUND, "U001", "해당 학교가 등록되어 있지 않습니다."),
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "P001", "게시글이 존재하지 않습니다."),
     ORGANIZATION_NOT_FOUND(HttpStatus.NOT_FOUND, "O002", "존재하지 않는 모임입니다."),
+    REPORT_NOT_FOUND(HttpStatus.NOT_FOUND, "R001", "해당 신고 내역이 존재하지 않습니다."),
 
     // 403
     ACCESS_DENIED_EDIT(HttpStatus.FORBIDDEN, "A001", "작성자만 수정할 수 있습니다."),
@@ -20,7 +21,9 @@ public enum ErrorCode {
     ACCESS_DENIED_PROFILE(HttpStatus.FORBIDDEN, "A005", "해당 프로필에 대한 접근 권한이 없습니다."),
 
     // 400
-    POST_IS_INACTIVE(HttpStatus.BAD_REQUEST, "P002", "비활성화된 게시글입니다."),
+    POST_IS_INACTIVE(HttpStatus.BAD_REQUEST, "P001", "비활성화된 게시글입니다."),
+    ALREADY_ACTIVE_POST(HttpStatus.BAD_REQUEST, "P002", "이미 활성화된 게시글입니다."),
+    CANNOT_ACTIVATE_DELETED_POST(HttpStatus.BAD_REQUEST, "P003", "삭제되지 않은 게시글은 활성화할 수 없습니다."),
 
     // 409 CONFLICT : 중복 데이터
     DUPLICATE_ORGANIZATION(HttpStatus.CONFLICT, "O001", "이미 존재하는 조직(동아리/소모임)입니다."),
